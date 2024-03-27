7eb6b779c3d0bd2eaeede566fdf36cabb2246488
Mamta Patil <mamtapatil44@gmail.com>
2024-03-27 17:26:10 -0400

    restro unavailble logic

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -10,8 +10,12 @@ const RestaurantMenu = () => {
   const { id } = useParams();
   const menuInfo = useRestroMenu(id);
   const dispatch = useDispatch();
-  if (menuInfo === null) return null;
+  if (!menuInfo) return null;
   let data = menuInfo?.cards[0]?.card?.card?.info;
+  if (!data)
+    return (
+      <div className=" min-h-full flex flex-row justify-between items-center lg:px-32 px-5 text-center bg-[url('assets/images/UNAVAILABLE.jpeg')] bg-no-repeat bg-cover bg-center"></div>
+    );
 
   const {
     name,
@@ -32,14 +36,13 @@ const RestaurantMenu = () => {
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-
-    const handleAddToCart =(item)=>{
-      if(!item?.card?.info?.cloudinaryImageId){
-        item.card.info.cloudinaryImageId  = cloudinaryImageId;
-      }
-     
-      dispatch(addItem(item))
+  const handleAddToCart = (item) => {
+    if (!item?.card?.info?.cloudinaryImageId) {
+      item.card.info.cloudinaryImageId = cloudinaryImageId;
     }
+
+    dispatch(addItem(item));
+  };
   return (
     <>
       <div className="flex h-56 bg-gray-900 text-white items-center">
@@ -56,74 +59,106 @@ const RestaurantMenu = () => {
               <FaStar color="green" />
               <span className="-mt-1">{avgRating}</span>
               <span className="-mt-1">
-             &nbsp; - &nbsp;{sla?.lastMileTravel} mi
+                &nbsp; - &nbsp;{sla?.lastMileTravel} mi
               </span>
             </h3>
           </span>
         </div>
       </div>
       <div className="flex items-center">
-        {!itemCards ?(<h1 className="mx-auto p-4 text-center font-bold">Currently not available </h1>):
-        <div className="mx-auto  justify-between">
-        <h3 className="font-bold mx-9 px-4 py-2">Recommended</h3>
-        {itemCards?.map((item) => (
-          <div className="h-auto m-12 p-4 shadow-lg" key={item?.card?.info?.id}>
-            <div className="hidden md:flex">
-              <span className="mr-4 pr-4 w-3/4">
-                <h3 className="font-bold">{item?.card?.info?.name}</h3>
-                <p>Rs. {item?.card?.info?.price ? item?.card?.info?.price/100 :item?.card?.info?.defaultPrice/100}</p>
-                <p className="text-wrap text-sm mt-2">
-                  {item?.card?.info?.description}
-                </p>
-              </span>
-              <span className=" overflow-hidden relative flex items-center justify-center flex-col p-3">
-                {item?.card?.info?.imageId ? (<img
-                  className="md:w-24 md:h-24 rounded-lg ml-20  object-cover"
-                  src={CDN_URL + item?.card?.info?.imageId}
-                  alt="menu-logo"
-                />) : <img
-                className="md:w-24 md:h-24  rounded-lg ml-20  object-cover"
-                src={CDN_URL + cloudinaryImageId}
-                alt="menu-logo"
-              />}
-                
-                <span className="absolute  flex  justify-center -pb-2"><button className="text-yellow-500 shadow-lg bg-white mx-2  my-4 px-2 rounded-lg absolute z-1 left-2 py-2"
-                onClick={()=>handleAddToCart(item)}>
-                  Add
-                </button></span>
-              </span>
-            </div>
+        {!itemCards ? (
+          <h1 className="mx-auto p-4 text-center font-bold">
+            Currently not available{" "}
+          </h1>
+        ) : (
+          <div className="mx-auto  justify-between">
+            <h3 className="font-bold mx-9 px-4 py-2">Recommended</h3>
+            {itemCards?.map((item) => (
+              <div
+                className="h-auto m-12 p-4 shadow-lg"
+                key={item?.card?.info?.id}
+              >
+                <div className="hidden md:flex">
+                  <span className="mr-4 pr-4 w-3/4">
+                    <h3 className="font-bold">{item?.card?.info?.name}</h3>
+                    <p>
+                      Rs.{" "}
+                      {item?.card?.info?.price
+                        ? item?.card?.info?.price / 100
+                        : item?.card?.info?.defaultPrice / 100}
+                    </p>
+                    <p className="text-wrap text-sm mt-2">
+                      {item?.card?.info?.description}
+                    </p>
+                  </span>
+                  <span className=" overflow-hidden relative flex items-center justify-center flex-col p-3">
+                    {item?.card?.info?.imageId ? (
+                      <img
+                        className="md:w-24 md:h-24 rounded-lg ml-20  object-cover"
+                        src={CDN_URL + item?.card?.info?.imageId}
+                        alt="menu-logo"
+                      />
+                    ) : (
+                      <img
+                        className="md:w-24 md:h-24  rounded-lg ml-20  object-cover"
+                        src={CDN_URL + cloudinaryImageId}
+                        alt="menu-logo"
+                      />
+                    )}
 
-            <div className="md:hidden">
-              <span className="mr-4 pr-4 w-full">
-                <h3 className="font-bold">{item?.card?.info?.name}</h3>  
-                <p>Rs. {item?.card?.info?.price ? item?.card?.info?.price/100 :item?.card?.info?.defaultPrice/100}</p> 
-              </span>
-              <span className=" overflow-hidden relative flex items-center justify-end px-1 py-3 flex-row">
-              
-                <p className="text-wrap text-sm mt-2">
-                  {item?.card?.info?.description}
-                </p>
-                {item?.card?.info?.imageId ? (<img
-                  className="w-20 h-20 rounded-lg ml-20  object-cover"
-                  src={CDN_URL + item?.card?.info?.imageId}
-                  alt="menu-logo"
-                />) : <img
-                className="w-20 h-20  rounded-lg ml-20  object-cover"
-                src={CDN_URL + cloudinaryImageId}
-                alt="menu-logo"
-              />}
-                
-                <span className="absolute align-bottom"><button className="text-yellow-500 shadow-lg bg-white  mb-0 px-2 rounded-lg  z-1  py-2"
-                onClick={()=>handleAddToCart(item)}>
-                  Add
-                </button></span>
-              </span>
-            </div>
+                    <span className="absolute  flex  justify-center -pb-2">
+                      <button
+                        className="text-yellow-500 shadow-lg bg-white mx-2  my-4 px-2 rounded-lg absolute z-1 left-2 py-2"
+                        onClick={() => handleAddToCart(item)}
+                      >
+                        Add
+                      </button>
+                    </span>
+                  </span>
+                </div>
+
+                <div className="md:hidden">
+                  <span className="mr-4 pr-4 w-full">
+                    <h3 className="font-bold">{item?.card?.info?.name}</h3>
+                    <p>
+                      Rs.{" "}
+                      {item?.card?.info?.price
+                        ? item?.card?.info?.price / 100
+                        : item?.card?.info?.defaultPrice / 100}
+                    </p>
+                  </span>
+                  <span className=" overflow-hidden relative flex items-center justify-end px-1 py-3 flex-row">
+                    <p className="text-wrap text-sm mt-2">
+                      {item?.card?.info?.description}
+                    </p>
+                    {item?.card?.info?.imageId ? (
+                      <img
+                        className="w-20 h-20 rounded-lg ml-20  object-cover"
+                        src={CDN_URL + item?.card?.info?.imageId}
+                        alt="menu-logo"
+                      />
+                    ) : (
+                      <img
+                        className="w-20 h-20  rounded-lg ml-20  object-cover"
+                        src={CDN_URL + cloudinaryImageId}
+                        alt="menu-logo"
+                      />
+                    )}
+
+                    <span className="absolute align-bottom">
+                      <button
+                        className="text-yellow-500 shadow-lg bg-white  mb-0 px-2 rounded-lg  z-1  py-2"
+                        onClick={() => handleAddToCart(item)}
+                      >
+                        Add
+                      </button>
+                    </span>
+                  </span>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>}
-        
+        )}
       </div>
     </>
   );
